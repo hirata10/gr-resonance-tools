13e44abb7e010933abbe8e9e858d1249d8c1a5c2
Makana Silva <silva.179@osu.edu>
2022-06-07 15:45:25 -0400

    changed function names

--- resonance_find.c
+++ resonance_find.c
@@ -116,7 +116,7 @@ double Omega_outer_direct(double radius, double M, double spin){
 	double omegaouter;
 
 	u = 1./radius;
-	omegaouter = sqrt(M*u*u*u)/(1. + spin*sqrt(M*u*u*u));
+	omegaouter = sqrt(M*u*u*u)/(1. + spin * M * sqrt(M*u*u*u));
 
 	return(omegaouter);
 
@@ -140,7 +140,9 @@ double Omega_outer_direct(double radius, double M, double spin){
 }*/
 
 /* Converts apocenter, pericenter, and inclination to Omega resonance condition */
-double ra_rp_I2Omega(int n, int k, int m, double radius, double ra, double rp, double I, double astar, double M){
+
+/* For outer body on a circular orbit, use this function. Only needs to be fed a mode vector for the inner orbit and the outer orbit radius. */
+double ra_rp_I2Omega_OuterCirc(int n, int k, int m, double radius, double ra, double rp, double I, double astar, double M){
 	double EQL[3];
 	double J[3];
 	double Minv[9];
@@ -153,37 +155,82 @@ double ra_rp_I2Omega(int n, int k, int m, double radius, double ra, double rp, d
 	CKerr_Minverse(J, Minv, M, astar);
 	CKerr_Minv2Omega(Minv, Omega_inner);
 	omega_out = Omega_outer_direct(radius, M, astar);
-	function = n*Omega_inner[0] + k*Omega_inner[1] + m*Omega_inner[2] - m*omega_out;
+	function = - n*Omega_inner[0] - k*Omega_inner[1] - m*Omega_inner[2] + m*omega_out;
+	return(function);
+}
+
+/* If you want a both orbits to be generic, then give apocenter, pericenter, and inclination for both */
+double ra_rp_I2Omega_generic(int n_inner, int k_inner, int m_inner, int n_outer, int k_outer, double ra_inner, double rp_inner, double I_inner, double ra_outer, double rp_outer, double I_outer, double astar, double M){
+	double EQL_inner[3], EQL_outer[3];
+	double J_inner[3], J_outer[3];
+	double Minv_inner[9], Minv_outer[9];
+	double Omega_inner[3];
+	double Omega_outer[3];
+	//double omega_out;
+	double function;
+
+	/* Inner Data */
+	if(rp_inner == 0 && I_inner == 0){
+		Omega_inner[0] = 0;
+		Omega_inner[1] = 0;
+		Omega_inner[3] = Omega_outer_direct(ra_inner, M, astar);
+	}
+	else{
+		ra_rp_I2EQL(ra_inner, EQL_inner, rp_inner, I_inner, astar, M);
+		CKerr_EQL2J(EQL_inner, J_inner, M, astar, NULL);
+		CKerr_Minverse(J_inner, Minv_inner, M, astar);
+		CKerr_Minv2Omega(Minv_inner, Omega_inner);
+	}
+	
+	/* Outer Data */
+	if(rp_outer == 0 && I_outer == 0){
+		Omega_outer[0] = 0;
+		Omega_outer[1] = 0;
+		Omega_outer[3] = Omega_outer_direct(ra_outer, M, astar);
+	}
+	else{
+		ra_rp_I2EQL(ra_outer, EQL_outer, rp_outer, I_outer, astar, M);
+		CKerr_EQL2J(EQL_outer, J_outer, M, astar, NULL);
+		CKerr_Minverse(J_outer, Minv_outer, M, astar);
+		CKerr_Minv2Omega(Minv_outer, Omega_outer);
+	}
+	
+
+	//omega_out = Omega_outer_direct(radius, M, astar);
+	//function = n*Omega_inner[0] + k*Omega_inner[1] + m*Omega_inner[2] - m*omega_out;
+	function = (n_outer*Omega_outer[0] - n_inner*Omega_inner[0]) + (k_outer*Omega_outer[1] - k_inner*Omega_inner[1]) + m_inner * (Omega_outer[2] - Omega_inner[2]);
+	//function = n*Omega_inner[0] + k*Omega_inner[1] + m*Omega_inner[2];
 	return(function);
 	//function = resonance_function(Omega_inner, radius, M, astar);
 }
 
-/* Bisection method for finding the apocenter of the resonance condition */
-double find_resonance_apo(int n, int k, int m, double radius, double guess1, double guess2, double rp, double I, double astar, double M){
+
+/* Bisection method for finding the apocenter of the resonance condition given the outer body is on a circular equatorial orbit */
+double find_resonance_apo_OuterCirc(int n, int k, int m, double radius, double guess1, double guess2, double rp, double I, double astar, double M){
 	double mid_apo=0;
 	double error=0;
 	int i=0;
 	double mold=0;
 
 
-	if(ra_rp_I2Omega(n, k, m, radius, guess1, rp, I, astar, M)*ra_rp_I2Omega(n, k, m, radius, guess2, rp, I, astar, M)>0){
-        	printf("Value at first and second guess: %lf, %lf \n", ra_rp_I2Omega(n, k, m, radius, guess1, rp, I, astar, M), ra_rp_I2Omega(n, k, m, radius, guess2, rp, I, astar, M));
+	if(ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess1, rp, I, astar, M)*ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess2, rp, I, astar, M)>0){
+        	printf("Value at first and second guess: %lf, %lf \n", ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess1, rp, I, astar, M), ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess2, rp, I, astar, M));
         	printf("Invalid Interval Exit! \n");       //to test whether search interval
         	exit(1);                                                        //is okay or not
 	}
-	else if(ra_rp_I2Omega(n, k, m, radius, guess1, rp, I, astar, M)==0 || ra_rp_I2Omega(n, k, m, radius, guess2, rp, I, astar, M)==0){
-        	printf("Value at first and second guess: %lf, %lf \n", ra_rp_I2Omega(n, k, m, radius, guess1, rp, I, astar, M), ra_rp_I2Omega(n, k, m, radius, guess2, rp, I, astar, M));
-        	printf("Root is one of interval bounds. Root is %f\n",ra_rp_I2Omega(n, k, m, radius, guess1, rp, I, astar, M)==0?guess1:guess2);
+	else if(ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess1, rp, I, astar, M)==0 || ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess2, rp, I, astar, M)==0){
+        	printf("Value at first and second guess: %lf, %lf \n", ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess1, rp, I, astar, M), ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess2, rp, I, astar, M));
+        	printf("Root is one of interval bounds. Root is %f\n",ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess1, rp, I, astar, M)==0?guess1:guess2);
         	exit(0);
 	}
 	printf("Ite\ta\t\tb\t\tmid_apo\t\tf(mid_apo)\t\terror\n");
 	do{
         	mold = mid_apo;
         	mid_apo = (guess1 + guess2)/2;
-        	printf("%2d\t%4.6f\t%4.6f\t%4.6f\t%lg\t",i++,guess1,guess2,mid_apo,ra_rp_I2Omega(n, k, m, radius, mid_apo, rp, I, astar, M));
-        	if(ra_rp_I2Omega(n, k, m, radius, mid_apo, rp, I, astar, M)==0){
+        	printf("%2d\t%4.6f\t%4.6f\t%4.6f\t%lg\t",i++,guess1,guess2,mid_apo,ra_rp_I2Omega_OuterCirc(n, k, m, radius, mid_apo, rp, I, astar, M));
+        	if(ra_rp_I2Omega_OuterCirc(n, k, m, radius, mid_apo, rp, I, astar, M)==0){
                 	printf("Root is %4.6f\n",mid_apo);
-        	}else if ((ra_rp_I2Omega(n, k, m, radius, guess1, rp, I, astar, M)*ra_rp_I2Omega(n, k, m, radius, mid_apo, rp, I, astar, M))<0){
+        	}else if ((ra_rp_I2Omega_OuterCirc(n, k, m, radius, guess1, rp, I, astar, M)*ra_rp_I2Omega_OuterCirc(n, k, m, radius, mid_apo, rp, I, astar, M))<0){
                 	guess2 = mid_apo;
         	}else guess1 = mid_apo;
         	error=fabs((mid_apo - mold)/mid_apo);
@@ -194,54 +241,4 @@ double find_resonance_apo(int n, int k, int m, double radius, double guess1, dou
 	printf("Approximate Root is %4.6f \n",mid_apo);
 	return(mid_apo);
 	}
-#if 0
-int main(){
-	int j;
-	int n, k, m;
-	double step, next_step, Omega_condition;
-	double apo_res, peri, incline, mass, spin, radius_outer, guess1, guess2;
-
-	printf("Enter pericenter: ");
-	scanf("%lf", &peri);
-	printf("Enter inlincation angle (radians): ");
-	scanf("%lf", &incline);
-	printf("Enter central mass: ");
-	scanf("%lf", &mass);
-	printf("Enter spin parameter of BH: ");
-	scanf("%lf", &spin);
-	printf("Enter radius of outer orbit: ");
-	scanf("%lf", &radius_outer);
-	//printf("Enter corresponding apocenter at resonance: ");
-	//scanf("%lf", &apo_res);
-
-	//printf("Number of modes and max n,k,m: ");
-	//scanf("%i %i %i %i", &nl, &nmax, &kmax, &mmax);
-
-	printf("Enter mode vector: ");
-	scanf("%i %i %i", &n, &k, &m);
-
-	//printf("Mode vector for outer orbit: ");
-	//scanf("%i %i %i", &n_res_outer, &k_res_outer, &m_res_outer);
-
-	guess1 = peri + 2.;
-	guess2 = radius_outer - 2.;
-
-	apo_res = find_resonance_apo(n, k, m, radius_outer, guess1, guess2, peri, incline, spin, mass);
-	printf("Apocenter for this resonance is = %lg \n", apo_res);
-	#if 0
-	for (j = 0; j <= 500; j++){
-		step = (guess2 - guess1)/500;
-		next_step = guess1 + step * j;
-		Omega_condition = ra_rp_I2Omega(n, k, m, radius_outer, next_step, peri, incline, spin, mass);
-		printf("%i %lg %lg \n", j, next_step, Omega_condition);
-	}
 	
-	printf("Delta_J_r_tidal Re & Im = %lg %lg \n", Delta_J_r_tidal[0], Delta_J_r_tidal[1]);
-	printf("Delta_J_theta_tidal = %lg %lg \n", Delta_J_theta_tidal[0], Delta_J_theta_tidal[1]);
-	printf("Delta_J_phi_tidal = %lg %lg \n", Delta_J_phi_tidal[0], Delta_J_phi_tidal[1]);
-	#endif
-	//for (j=0;j<nl*nmax*kmax*mmax;j++){printf("%2d \t\t %19.12lE \n", j, J_dot_r[j]);}
-	return(0);
-}
-#endif
-
