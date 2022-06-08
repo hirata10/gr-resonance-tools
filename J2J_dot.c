e238cd0fd4574394a023dabf8afedfb6674dc2fc
Makana Silva <silva.179@osu.edu>
2022-06-08 13:27:14 -0400

    Updated RK4 method

--- J2J_dot.c
+++ J2J_dot.c
@@ -37,13 +37,25 @@ int J2Jdot_component(int nl, int nmax, int kmax, int mmax, double J_r_ini, doubl
 /* This takes a step size (dt), an inital J_inital components, and return arrays of size n for each J components at each time step */
 void rk4_J2Jdot(double dt, double t0, int n, double J_r_ini, double J_theta_ini, double J_phi_ini, double *J_r_final, double *J_theta_final, double *J_phi_final){
     int i;
-    int nl = 2, nmax = 1, kmax = 1, mmax = 1;
+    int nl = 1, nmax = 1, kmax = 1, mmax = 1;
     double M = 1, astar = 0.5;
-	double *k1r, *k2r, *k3r, *k4r;
-    double *k1theta, *k2theta, *k3theta, *k4theta;
-    double *k1phi, *k2phi, *k3phi, *k4phi;
-    double *J_dot_r, *J_dot_theta, *J_dot_phi;
-	double *t;
+    double k1r, k2r, k3r, k4r;
+    double k1theta, k2theta, k3theta, k4theta;
+    double k1phi, k2phi, k3phi, k4phi;
+    double J_dot_r, J_dot_theta, J_dot_phi;
+    // double *k1r, *k2r, *k3r, *k4r;
+    // double *k1theta, *k2theta, *k3theta, *k4theta;
+    // double *k1phi, *k2phi, *k3phi, *k4phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r, *J_dot_theta, *J_dot_phi;
+    // double *J_dot_r0, *J_dot_theta0, *J_dot_phi0;
+    double *t;
 
     /* Define the array for the final solution, they should start at the inital conditions given */
     J_r_final[0] = J_r_ini;
@@ -51,78 +63,119 @@ void rk4_J2Jdot(double dt, double t0, int n, double J_r_ini, double J_theta_ini,
     J_phi_final[0] = J_phi_ini;
 
     /* Define time step array, starts at inital time (input) */
-	t = (double *)malloc(sizeof(double) * n);
-	t[0] = t0;
+    t = (double *)malloc(sizeof(double) * n);
+    t[0] = t0;
 
-	/*
+    /*
     J_dot_r = (double *)malloc(sizeof(double) * n);
     J_dot_theta = (double *)malloc(sizeof(double) * n);
     J_dot_phi = (double *)malloc(sizeof(double) * n);
     */
 
     /* Create slopes for each direction r,theta,phi */
-	k1r = (double *)malloc(sizeof(double) * n);
-	k2r = (double *)malloc(sizeof(double) * n);
-	k3r = (double *)malloc(sizeof(double) * n);
-	k4r = (double *)malloc(sizeof(double) * n);
+    #if 0
+    k1r = (double *)malloc(sizeof(double) * n);
+    k2r = (double *)malloc(sizeof(double) * n);
+    k3r = (double *)malloc(sizeof(double) * n);
+    k4r = (double *)malloc(sizeof(double) * n);
 
     k1theta = (double *)malloc(sizeof(double) * n);
-	k2theta = (double *)malloc(sizeof(double) * n);
-	k3theta = (double *)malloc(sizeof(double) * n);
-	k4theta = (double *)malloc(sizeof(double) * n);
+    k2theta = (double *)malloc(sizeof(double) * n);
+    k3theta = (double *)malloc(sizeof(double) * n);
+    k4theta = (double *)malloc(sizeof(double) * n);
 
     k1phi = (double *)malloc(sizeof(double) * n);
-	k2phi = (double *)malloc(sizeof(double) * n);
-	k3phi = (double *)malloc(sizeof(double) * n);
-	k4phi = (double *)malloc(sizeof(double) * n);
+    k2phi = (double *)malloc(sizeof(double) * n);
+    k3phi = (double *)malloc(sizeof(double) * n);
+    k4phi = (double *)malloc(sizeof(double) * n);
+    #endif
+    printf("i \t t \t J_r \t J_theta \t J_phi \n------------\n");
+    //printf("%i \t%lf \t%lf \t%lf \t%lf \t%lf \t %lf \n", 0, t[0], J_r_final[0], J_theta_final[0], J_phi_final[0], k1r[0], k1theta[0]);
+    
+    for (i = 1; i < (n + 1); i++){
+        /* k1_{r,theta,phi} will come from the same call of J2Jdot_component since no changes start yet */
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k1r = dt * (J_dot_r);
+        k1theta = dt * (J_dot_theta);
+        k1phi = dt * (J_dot_phi);
 
-	for (i = 1; i < n; i++)
-		{
-            /* k1_{r,theta,phi} will come from the same call of J2Jdot_component since no changes start yet */
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k1r[i-1] = dt * (*J_dot_r);
-            k1theta[i-1] = dt * (*J_dot_theta);
-            k1phi[i-1] = dt * (*J_dot_phi);
-
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k1r[i-1]/2, J_theta_final[i-1], J_phi_final[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k2r[i-1] = dt * (*J_dot_r);
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k1theta[i-1]/2, J_phi_final[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k2theta[i-1] = dt * (*J_dot_theta);
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k1phi[i-1]/2, J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k2phi[i-1] = dt * (*J_dot_phi);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k1r/2., J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k2r= dt * (J_dot_r);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k1theta/2., J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k2theta = dt * (J_dot_theta);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k1phi/2., &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k2phi = dt * (J_dot_phi);
            
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k2r[i-1]/2, J_theta_final[i-1], J_phi_final[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k3r[i-1] = dt * (*J_dot_r);
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k2theta[i-1]/2, J_phi_final[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k3theta[i-1] = dt * (*J_dot_theta);
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k2phi[i-1]/2, J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k3phi[i-1] = dt * (*J_dot_phi);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k2r/2., J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k3r = dt * (J_dot_r);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k2theta/2., J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k3theta = dt * (J_dot_theta);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k2phi/2., &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k3phi = dt * (J_dot_phi);
 
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k3r[i-1], J_theta_final[i-1], J_phi_final[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k4r[i-1] = dt * (*J_dot_r);
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k3theta[i-1], J_phi_final[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k4theta[i-1] = dt * (*J_dot_theta);
-            J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k3phi[i-1], J_dot_r, J_dot_theta, J_dot_phi, M, astar);
-			k4phi[i-1] = dt * (*J_dot_phi);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k3r, J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k4r = dt * (J_dot_r);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k3theta, J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k4theta = dt * (J_dot_theta);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k3phi, &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k4phi = dt * (J_dot_phi);
 
-			t[i] = t[i-1] + dt;
-			J_r_final[i] = J_r_final[i-1] + (k1r[i-1] + 2 * k2r[i-1] + 2 * k3r[i-1] + k4r[i-1]) / 6;
-            J_theta_final[i] = J_theta_final[i-1] + (k1theta[i-1] + 2 * k2theta[i-1] + 2 * k3theta[i-1] + k4theta[i-1]) / 6;
-            J_phi_final[i] = J_phi_final[i-1] + (k1phi[i-1] + 2 * k2phi[i-1] + 2 * k3phi[i-1] + k4phi[i-1]) / 6;
-			//printf("%i\t %g\t \n", i, x[i]);
-		}
+        t[i] = t[i-1] + dt;
+        J_r_final[i] = J_r_final[i-1] + (k1r + 2. * k2r + 2. * k3r + k4r) / 6.;
+        J_theta_final[i] = J_theta_final[i-1] + (k1theta + 2. * k2theta + 2. * k3theta + k4theta) / 6.;
+        J_phi_final[i] = J_phi_final[i-1] + (k1phi + 2. * k2phi + 2. * k3phi + k4phi) / 6.;
+        //printf("%i\t %g\t \n", i, x[i]);
+		printf("%i \t%f \t%e \t%e \t%e \t%e \t %e \n", i-1, t[i-1], J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1], k1r, k1theta);
+    }
+    free((char*)t);
+    #if 0
+    for (i = 1; i < (n+1); i++){
+        /* k1_{r,theta,phi} will come from the same call of J2Jdot_component since no changes start yet */
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k1r[i-1] = dt * (J_dot_r);
+        k1theta[i-1] = dt * (J_dot_theta);
+        k1phi[i-1] = dt * (J_dot_phi);
 
-	free((char*)t);
-	free((char*)k1r);
- 	free((char*)k1theta);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k1r[i-1]/2, J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k2r[i-1] = dt * (J_dot_r);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k1theta[i-1]/2, J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k2theta[i-1] = dt * (J_dot_theta);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k1phi[i-1]/2, &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k2phi[i-1] = dt * (J_dot_phi);
+           
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k2r[i-1]/2, J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k3r[i-1] = dt * (J_dot_r);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k2theta[i-1]/2, J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k3theta[i-1] = dt * (J_dot_theta);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k2phi[i-1]/2, &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k3phi[i-1] = dt * (J_dot_phi);
+
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1] + k3r[i-1], J_theta_final[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k4r[i-1] = dt * (J_dot_r);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1] + k3theta[i-1], J_phi_final[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k4theta[i-1] = dt * (J_dot_theta);
+        J2Jdot_component(nl, nmax, kmax, mmax, J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1] + k3phi[i-1], &J_dot_r, &J_dot_theta, &J_dot_phi, M, astar);
+        k4phi[i-1] = dt * (J_dot_phi);
+
+        t[i] = t[i-1] + dt;
+        J_r_final[i] = J_r_final[i-1] + (k1r[i-1] + 2 * k2r[i-1] + 2 * k3r[i-1] + k4r[i-1]) / 6;
+        J_theta_final[i] = J_theta_final[i-1] + (k1theta[i-1] + 2 * k2theta[i-1] + 2 * k3theta[i-1] + k4theta[i-1]) / 6;
+        J_phi_final[i] = J_phi_final[i-1] + (k1phi[i-1] + 2 * k2phi[i-1] + 2 * k3phi[i-1] + k4phi[i-1]) / 6;
+        //printf("%i\t %g\t \n", i, x[i]);
+		printf("%i \t%lf \t%lf \t%lf \t%lf \t%lf \t %lf \n", i-1, t[i-1], J_r_final[i-1], J_theta_final[i-1], J_phi_final[i-1], k1r[i-1], k1theta[i-1]);
+    }
+    free((char*)t);
+    free((char*)k1r);
+    free((char*)k1theta);
     free((char*)k1phi);
- 	free((char*)k2r);
- 	free((char*)k2theta);
+    free((char*)k2r);
+    free((char*)k2theta);
     free((char*)k2phi);
- 	free((char*)k3r);
- 	free((char*)k3theta);
+    free((char*)k3r);
+    free((char*)k3theta);
     free((char*)k3phi);
- 	free((char*)k4r);
- 	free((char*)k4theta);
+    free((char*)k4r);
+    free((char*)k4theta);
     free((char*)k4phi);
+    #endif
 }
